e25885961984596afc0275bb45c8849d5f563201
Jaspreet Singh <jaspreetsingh2379@gmail.com>
2020-04-22 13:44:17 +0530

    Changed Format ,added single time, added audio/video

---
+++ README.md
@@ -0,0 +1,5 @@
+# Youtube Downloder Backend
+
+I used `ytdl-core` and `micro`(from ZEIT Now) to develop a simple backend function
+to get info about a youtube video including a url to download video, which is just url
+of S3 or Cloud storage URL of youtube video.
--- src/app.js
+++ src/app.js
@@ -1,30 +1,53 @@
+const { calcSize, calcTime } = require("./timeCalc");
 const { send } = require("micro");
 const ytdl_C = require("ytdl-core");
 const { promisify } = require("util");
 const microCors = require("micro-cors");
 const cors = microCors({ allowMethods: ["GET"] });
-
 const getInfo = promisify(ytdl_C.getInfo);
 
-const polishFormats = (formats) => {
-  return formats
-    .filter((x) => x.container === "mp4")
-    .map((x) => {
-      return {
-        q: x.qualityLabel,
-        size: x.bitrate / 1024,
-        dUrl: x.url,
-        ext: x.container,
-        duration: x.approxDurationMs / 1000 / 60,
-      };
-    });
+const extractInfo = (x) => {
+  return {
+    q: x.qualityLabel || x.quality,
+    size: calcSize(x.bitrate, x.approxDurationMs),
+    dUrl: x.url,
+  };
 };
 
-const getInfoOfVideo = async (videoID) => {
+const polishFormats = (formats, reqFormat) => {
+  const videos = [];
+  const audios = [];
+
+  let time = null;
+
+  for (let i = 0; i < formats.length; i++) {
+    const { container, mimeType, approxDurationMs } = formats[i];
+    if (container === "mp4") {
+      const type = mimeType.split("/")[0].toLowerCase();
+      if (!time) {
+        time = calcTime(approxDurationMs);
+      }
+
+      if (reqFormat === "video") {
+        if (type === "video") videos.push(extractInfo(formats[i]));
+      } else if (reqFormat === "audio") {
+        if (type === "audio") audios.push(extractInfo(formats[i]));
+      } else {
+        if (type === "video") videos.push(extractInfo(formats[i]));
+        if (type === "audio") audios.push(extractInfo(formats[i]));
+      }
+    }
+  }
+
+  return { videos, audios, time, ext: "mp4" };
+};
+
+const getInfoOfVideo = async (videoID, reqFormat) => {
   try {
     const info = await getInfo(videoID);
-    return polishFormats(info.formats);
+    return polishFormats(info.formats, reqFormat);
   } catch (error) {
+    console.log("ERROR", error);
     throw new Error("Error getting data for videoID");
   }
 };
@@ -37,7 +60,8 @@ const getData = async ({ method, url }) => {
 
   const videoID = searchParams.get("id");
   if (!videoID) throw new Error("pass id as query param");
-  return await getInfoOfVideo(videoID);
+  const reqFormat = searchParams.get("format");
+  return await getInfoOfVideo(videoID, reqFormat);
 };
 
 const handler = async (req, res) => {
@@ -45,7 +69,7 @@ const handler = async (req, res) => {
 
   try {
     const data = await getData(req);
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Origin", "*");
     return send(res, 200, { error: false, msg: "Video Info", data });
   } catch (error) {
     return send(res, 400, { error: true, msg: error.message });
